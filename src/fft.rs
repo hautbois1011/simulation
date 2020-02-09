51040cfd5c240162ac6bbcc312c7b1efa956bd19
hautbois1011 <subterranean_mirror@yahoo.co.jp>
2020-02-10 00:58:22 +0900

    reduce memory usage

--- src/fft.rs
+++ src/fft.rs
@@ -4,15 +4,13 @@ pub fn fft(input: &Vec<Complex>, inverse: bool) -> Vec<Complex> {
     let len = input.len();
     let l = (len as f64).log2().ceil() as u32;
     let n = 2u32.pow(l) as usize;
-    let mut x = vec![vec![Complex::new(0.0, 0.0); n]; (l + 1) as usize];
-    for i in 0..len {
-        x[0][i] = input[i];
-    }
+    let mut x = input.clone();
+    let y = &mut vec![Complex::new(0.0, 0.0); n];
 
     let mut p = n >> 1;
     let mut r = 1;
 
-    for i in 0..l as usize {
+    for _ in 0..l as usize {
         let mut twiddle = (0..p)
             .map(|k| Complex::exp2pi(-(k as f64) / (2. * p as f64)))
             .collect::<Vec<Complex>>();
@@ -23,18 +21,18 @@ pub fn fft(input: &Vec<Complex>, inverse: bool) -> Vec<Complex> {
 
         for r_hat in 0..r as usize {
             for p_hat in 0..p as usize {
-                x[i + 1][2 * r * p_hat + r_hat] =
-                    x[i][r * p_hat + r_hat] + x[i][p * r + r * p_hat + r_hat];
-                x[i + 1][2 * r * p_hat + r + r_hat] =
-                    twiddle[p_hat] * (x[i][r * p_hat + r_hat] - x[i][p * r + r * p_hat + r_hat]);
+                y[2 * r * p_hat + r_hat] = x[r * p_hat + r_hat] + x[p * r + r * p_hat + r_hat];
+                y[2 * r * p_hat + r + r_hat] =
+                    twiddle[p_hat] * (x[r * p_hat + r_hat] - x[p * r + r * p_hat + r_hat]);
             }
         }
 
         p >>= 1;
         r <<= 1;
+        x = y.clone();
     }
 
-    x[l as usize].clone()
+    x.clone()
 }
 
 pub fn real_fft(input: &Vec<f64>) -> Vec<Complex> {
